844c01b24e97888e3eac5a182dba6bda391543ec
leo9895 <leoklawrence037@gmail.com>
2025-11-24 10:25:24 +0530

    Add FormsModule to AppModule imports

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+// src/app/app.module.ts
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';           // <-- add this
+
+import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculatorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule        // <-- add here
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
